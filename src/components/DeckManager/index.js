856e40cee533a56bc7ac87028f43ad1c50352987
Jiaming <hfter789@gmail.com>
2016-11-16 21:27:16 -0800

    style enhencement

---
+++ src/components/DeckManager/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class DeckManager extends Component {
+  render() {
+    return (
+      <div>
+        Deck Manager
+      </div>
+    );
+  }
+}
+
+export default DeckManager;
